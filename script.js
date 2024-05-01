e83de6e994da06b07c4f6bf5408e0b57019b6d82
AmanKaushik975 <91477500+AmanKaushik975@users.noreply.github.com>
2024-05-01 21:05:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 //your code here
+let dragElem = null;
+
+function addEventListeners(div) {
+    div.addEventListener("dragstart", e => {
+        console.log("dragstart event fired for div with id:", e.target.id);
+        dragElem = e.target;
+    });
+
+    div.addEventListener("dragover", e => {
+        e.preventDefault(); // This is necessary to allow dropping
+    });
+
+    div.addEventListener("drop", e => {
+        e.preventDefault(); // This is necessary to allow dropping
+
+        // Swap the divs
+        let temp = e.target.outerHTML;
+        e.target.outerHTML = dragElem.outerHTML;
+        dragElem.outerHTML = temp;
+
+        // Add event listeners to the new divs
+        addEventListeners(e.target);
+        addEventListeners(dragElem);
+
+        dragElem = null;
+    });
+}
+
+// Get all divs
+let divs = document.getElementsByClassName("image");
+
+// Add event listeners to all divs
+for(let i=0; i<divs.length; i++) {
+    addEventListeners(divs[i]);
+}
